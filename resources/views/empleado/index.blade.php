274e9b411df5d0340a4caa4237fe04c3e821fb39
Emmanuel <emmanuelrodruiguez7658@gmail.com>
2024-11-07 20:17:04 -0600

    Initial commit

---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,30 @@
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-primary"
+    >
+        <thead>
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Foto</th>
+                <th scope="col">Apellido Paterno</th>
+                <th scope="col">Apellido Materno</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">R1C1</td>
+                <td>R1C2</td>
+                <td>R1C3</td>
+            </tr>
+            <tr class="">
+                <td scope="row">Item</td>
+                <td>Item</td>
+                <td>Item</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
